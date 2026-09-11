87520b10aaab7bbed07f7c83429cc499acbd3a81
msfmfjt <msfmfjt@gmail.com>
2026-09-11 16:11:01 +0100

    P1: add compact C2 payoff opcodes

--- crates/pricing-product/src/lib.rs
+++ crates/pricing-product/src/lib.rs
@@ -3,12 +3,14 @@
 #![forbid(unsafe_code)]
 
 mod graph;
+mod smoothing;
 mod spec;
 
 pub use graph::{
     CompiledOpcode, CompiledPayoff, GraphError, GraphFingerprint, GraphLimitPolicy,
     PayoffEvaluation, SourceGraph, SourceGraphBuilder, SourceNode, SourceOpcode, TerminalAdjoint,
 };
+pub use smoothing::{CompactC2Smoothing, SmoothingBinaryDerivatives, SmoothingDerivatives};
 pub use spec::{
     ArithmeticAsianSpec, AsianObservation, AsianObservationValue, BarrierDirection, BarrierSpec,
     BarrierStyle, DigitalPayout, DigitalSpec, EuropeanVanillaSpec, FixedLookbackSpec, OptionSide,
